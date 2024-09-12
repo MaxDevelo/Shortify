a1305329a8581fcca6e72e6ed244ec5bccdfce0b
MaxDevelo <maxime.wawrzy@gmail.com>
2024-09-12 20:15:42 +0200

    feat(project): add debug tool bar + update form

--- Shortify/src/Controller/ShortLinkController.php
+++ Shortify/src/Controller/ShortLinkController.php
@@ -4,17 +4,28 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Form\ShortLinkType;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ShortLinkController extends AbstractController
 {
     #[Route('/short/link', name: 'app_short_link')]
-    public function index(): Response
+    public function index(Request $request, LoggerInterface $logger): Response
     {
+        $formShortLink = $this->createForm(ShortLinkType::class);
+
+        $formShortLink->handleRequest($request);
+
+        if ($formShortLink->isSubmitted() && $formShortLink->isValid()) {
+            $logger->info('Your form submitted !');
+        }
+
         return $this->render('short_link/index.html.twig', [
-            'controller_name' => 'ShortLinkController',
+            'form_short_link' => $formShortLink,
         ]);
     }
 }
--- Shortify/src/Form/ShortLinkType.php
+++ Shortify/src/Form/ShortLinkType.php
@@ -1,32 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Form;
 
 use App\Entity\ShortLink;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Url;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ShortLinkType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('long_url')
-            ->add('short_link')
-            ->add('created_at', null, [
+            ->add('long_url', UrlType::class, [
+                'label' => 'URL',
+                'constraints' => [
+                    new Url([
+                        'message' => 'Please enter a valid URL.',
+                    ])
+                    ],
+            ])
+            ->add('expires_at', DateType::class, [
                 'widget' => 'single_text',
             ])
-            ->add('expires_at', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('users', EntityType::class, [
-                'class' => User::class,
-'choice_label' => 'id',
-            ])
-        ;
+            ->add('save', SubmitType::class, ['label' => 'Generate short link']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
