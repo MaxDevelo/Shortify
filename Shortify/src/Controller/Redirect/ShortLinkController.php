c2c4e7e227b803f027a1383f9f0570b7aa57e05b
MaxDevelo <maxime.wawrzy@gmail.com>
2024-09-13 21:39:09 +0200

    feat(redirect): add controller for redirect shortlink + add timer before click link

---
+++ Shortify/src/Controller/Redirect/ShortLinkController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Redirect;
+
+use App\Repository\ShortLinkRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ShortLinkController extends AbstractController
+{    
+    /**
+     * @param ShortLinkRepository $shortLinkRepository
+     * @return void
+     */
+    public function __construct (
+        private ShortLinkRepository $shortLinkRepository
+    ) {}
+
+    #[Route('/{shortLink}', name: 'app_redirect_short_link')]
+    public function index(string $shortLink): Response
+    {
+        $shortLink = $this->shortLinkRepository->verifyShortLinkExists('/' . $shortLink);
+
+        if(!$shortLink) {
+         return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('short_link/redirect/index.html.twig', [
+            'url' => $shortLink->getLongUrl(),
+        ]);
+    }
+}
--- Shortify/src/Controller/ShortLinkGeneratorController.php
+++ Shortify/src/Controller/ShortLinkGeneratorController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class ShortLinkController extends AbstractController
+class ShortLinkGeneratorController extends AbstractController
 {    
     /**
      * @param LoggerInterface $logger
